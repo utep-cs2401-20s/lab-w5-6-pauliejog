fa7a94bea6f540c0963ba702b25c6d03516570f5
pauliejog <Elakpjd7!>
2020-02-24 14:48:17 -0700

    found a bug in findTailExh with second test case

--- java/src/SnakeGameTester.java
+++ java/src/SnakeGameTester.java
@@ -5,14 +5,35 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class SnakeGameTester {
+
+    /*
+     * findTailExhaustive:
+     * Test case 1:
+     * findTailExhaustive() → exp
+     */
     @Test
     public void testFindTailExh1() {
         boolean[][] a = {{true,true,true},
                         {true,false,true},
                         {false,false,true}};
         int[] exp = {1,0,6};
-        
         SnakeGame sg = new SnakeGame(a,2,2);
+        assertArrayEquals(exp,sg.findTailExhaustive());
+    }
+
+    /*
+     * findTailExhaustive:
+     * Test case 2:
+     * findTailExhaustive() → exp
+     */
+    @Test
+    public void testFindTailExh2() {
+        boolean[][] a = {{false,true,true,false},
+                        {false,false,true,false},
+                        {false,true,true,false},
+                        {false,true,false,false}};
+        int[] exp = {3,1,6};
+        SnakeGame sg = new SnakeGame(a,0,1);
         assertArrayEquals(exp,sg.findTailExhaustive());
     }
 }
